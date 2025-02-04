7f425fbb46e10c825a5731e7f10bc75d3b000041
Wealth <shuaibganiyat20@gmail.com>
2025-02-04 12:54:13 +0100

    made some changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { ClassifyNumberRouter } from './routes/classifyNumberRoutes.js';
+import { ClassifyNumberRouter } from './src/routes/classifyNumberRoutes.js';
 
 dotenv.config();
 const app = express();
